07677006a035f504f2631f8eec10ec10b5c46bcd
Siddhy <siddhy@kanyounkou.com>
2018-01-20 12:30:02 +0100

    add rancher port udp

--- install/firewall.sh
+++ install/firewall.sh
@@ -22,10 +22,10 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 # Services that the system will offer to the network + luc
 TCP_SERVICES="22 80 443" # SSH 
-UDP_SERVICES=""
+UDP_SERVICES="500 4500" # rancher port
 # Services the system will use from the network
 REMOTE_TCP_SERVICES="22 53 80 443" # web browsing
-REMOTE_UDP_SERVICES="53 123" # DNS
+REMOTE_UDP_SERVICES="53 123 500 4500" # DNS
 # FTP backups 
 # Allow backups to an external FTP
 FTP_BACKUPS=""
